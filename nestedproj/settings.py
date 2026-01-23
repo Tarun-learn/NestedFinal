41185aa2c19b949820d811344354e71b8f5054ed
Tarun-learn <116tarun@gmail.com>
2026-01-23 16:55:59 +0530

    db

--- nestedproj/settings.py
+++ nestedproj/settings.py
@@ -89,14 +89,14 @@ WSGI_APPLICATION = 'nestedproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE' : 'django.db.backends.mysql',
-        'NAME' : 'newnestedapidb2',           #  dtabase name          #dtabase name
-        'USER' : 'root',
-        'PASSWORD': '0987654321',
-        'HOST':'127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE' : 'django.db.backends.mysql',
+        # 'NAME' : 'newnestedapidb2',           #  dtabase name          #dtabase name
+        # 'USER' : 'root',
+        # 'PASSWORD': '0987654321',
+        # 'HOST':'127.0.0.1',
+        # 'PORT': '3306',
     }
 }
 
